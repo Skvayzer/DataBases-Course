eacd3f69e8fe2c91d08004360b2fb6efbbab8ca1
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-04-17 19:22:32 +0300

    lab9

--- week9/ex2.sql
+++ week9/ex2.sql
@@ -4,6 +4,15 @@ LANGUAGE plpgsql
 AS $$
 
 BEGIN
+   if starting < 0 then
+        raise exception 'The start index cannot be negative.';
+   end if;
+   if ending > 600 then
+        raise exception 'The end index cannot be larger than 600.';
+   end if;
+   if starting > ending then
+        raise exception 'The start index cannot be larger than the end index.';
+   end if;
    return query select *
    from customer as a
    where address_id between starting and ending
