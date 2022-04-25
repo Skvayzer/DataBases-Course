954a5aa38c69c62cf0598d7c0842fea0043ec398
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-04-25 23:44:17 +0300

    lab11

--- week11/ex1.py
+++ week11/ex1.py
@@ -1,162 +1,35 @@
-import psycopg2
+from pymongo import MongoClient
 
-con = psycopg2.connect(database="Week11", user="postgres",
-                       password="postgres", host="127.0.0.1", port="5432")
-con.autocommit = False
-cur = con.cursor()
-print("Database opened successfully, no autocommit mode.")
+client = MongoClient("mongodb://localhost")
 
-cur.execute("DROP TABLE IF EXISTS account")
-cur.execute("DROP TABLE IF EXISTS Ledger")
-con.commit()
+db = client['test']
 
-com = """CREATE TABLE account (
-    id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    credit VARCHAR(50),
-    currency INT
-);"""
-cur.execute(com)
-con.commit()
+def ex1_1():
+      cursor = db.restaurants.find({'cuisine':'Indian'})
+      for document in cursor:
+            print(document)
 
-com = """CREATE TABLE Ledger (
-    id SERIAL PRIMARY KEY,
-    from_ INT NOT NULL,
-    to_ INT NOT NULL,
-    fee INT NOT NULL,
-    amount INT NOT NULL,
-    transaction_date_time date NOT NULL
-);"""
-cur.execute(com)
-con.commit()
+def ex1_2():
+      cursor = db.restaurants.find({
+          "$or": [{
+              "cuisine": 'Indian'
+          }, {
+              "cuisine": 'Thai'
+          }]
+      })
+      for document in cursor:
+            print(document)
 
-names = ['Ivan', "Anna", "Maruf"]
+def ex1_3():
+      cursor = db.restaurants.find({
+            'address.building': '1115',
+            'address.street': 'Rogers Avenue',
+            'address.zipcode': '11226'
+      })
 
-for i in range(len(names)):
-    com1 = f"""INSERT INTO account (id, name, credit, currency)
-            VALUES ({i + 1}, '{names[i]}', 'RUB', 1000); """
-    cur.execute(com1)
-    con.commit()
+      for document in cursor:
+            print(document)
 
-cur.execute("SELECT * FROM account;")
-print(cur.fetchall())
-
-trans_sums = [500, 700, 100]
-trans_from_to = [(1, 3), (2, 1), (2, 3)]
-trans_list = []
-
-for sum, dest in zip(trans_sums, trans_from_to):
-    trans_list.append(f"""
-                        UPDATE account
-                            SET currency = currency - {sum}
-                            WHERE id = {dest[0]};
-                        
-                        UPDATE account
-                            SET currency = currency + {sum}
-                            WHERE id = {dest[1]}; 
-                        
-                        INSERT INTO Ledger (from_, to_, fee, amount, transaction_date_time) 
-                        VALUES (
-                            {dest[0]}, 
-                            {dest[1]}, 
-                            {0}, 
-                            {sum}, 
-                            (SELECT CURRENT_DATE)
-                        );
-                        """)
-for i, trans in zip(range(len(trans_list)), trans_list):
-    cur.execute("BEGIN;")
-    cur.execute(f"SAVEPOINT T{i};")
-    cur.execute(trans)
-
-    print('-' * 60)
-    print("Accounts currency:")
-    cur.execute("SELECT currency from account ORDER BY id;")
-    print(cur.fetchall())
-
-    print('-' * 40)
-    print("Ledger entries:")
-    cur.execute("SELECT from_, to_, fee, amount  FROM Ledger;")
-
-    output = cur.fetchall()
-    for line in output:
-        print(line)
-
-    cur.execute(f"ROLLBACK TO T{i}")
-
-    cur.execute("COMMIT;")
-
-# Adding new column
-cur.execute("""ALTER TABLE account ADD COLUMN BankName VARCHAR(50);""")
-con.commit()
-
-banks_names = ["Sber", "Tinkoff", "Sber"]
-for i in range(3):
-    cur.execute(f"""UPDATE account SET BankName = '{banks_names[i]}' WHERE id = {i + 1};""")
-    con.commit()
-
-cur.execute("INSERT INTO account (id, name, credit, currency) VALUES (4, 'sber_to_tink', 'USD', '30')")
-con.commit()
-
-print('~'*60)
-print("Database with Bank names:")
-cur.execute("SELECT * FROM account;")
-for row in cur.fetchall():
-    print(row)
-
-trans_list = []
-for sum, dest in zip(trans_sums, trans_from_to):
-    trans_list.append(f"""
-                        SAVEPOINT rollback;
-                        
-                        UPDATE account
-                            SET currency = currency - {sum}
-                             WHERE id = {dest[0]};
-                  
-                        UPDATE account AS acc
-                            SET currency = acc.currency - fee.currency
-                            FROM account AS fee 
-                            WHERE acc.id = {dest[0]} 
-                                AND fee.id = {4} 
-                                AND acc.BankName NOT IN (SELECT BankName FROM account WHERE id = {dest[1]});
-                        
-                        UPDATE account
-                            SET currency = currency + {sum}
-                            WHERE id = {dest[1]};  
-                        
-                        INSERT INTO Ledger (from_, to_, fee, amount, transaction_date_time) 
-                        VALUES (
-                            {dest[0]}, 
-                            {dest[1]}, 
-                            CASE 
-                                WHEN (SELECT BankName FROM account WHERE id = {dest[0]}) != (SELECT BankName FROM account WHERE id = {dest[1]})
-                                    THEN (SELECT currency FROM account WHERE id = {4})
-                                ELSE 0
-                            END,
-                            {sum}, 
-                            (SELECT CURRENT_DATE)
-                        );""")
-
-for i, trans in zip(range(len(trans_list)), trans_list):
-    cur.execute("BEGIN;")
-    cur.execute(f"SAVEPOINT T{i};")
-    cur.execute(trans)
-
-    print('-' * 60)
-    print("Accounts currency:")
-    cur.execute("SELECT currency from account ORDER BY id;")
-    print(cur.fetchall())
-
-    print('-' * 40)
-    print("Ledger entries:")
-    cur.execute("SELECT from_, to_, fee, amount  FROM Ledger;")
-
-    output = cur.fetchall()
-    for line in output:
-        print(line)
-
-    cur.execute(f"ROLLBACK TO T{i}")
-
-    cur.execute("COMMIT;")
-
-print('-' * 60)
+ex1_1()
+# ex1_2()
+# ex1_3()
--- week11/ex2.py
+++ week11/ex2.py
@@ -1,40 +1,24 @@
-import psycopg2
+from pymongo import MongoClient
+import datetime
+client = MongoClient("mongodb://localhost")
 
-con = psycopg2.connect(database="Week11", user="postgres",
-                       password="postgres", host="127.0.0.1", port="5432")
-con.autocommit = False
-cur = con.cursor()
-print("Database opened successfully, no autocommit mode.")
+db = client['test']
 
+def ex2():
+    result = db.restaurants.insert_one({'address': {
+                'building': '1480',
+                'street': '2 Avenue',
+                'zipcode': '10075',
+                'coords': [-73.9557413, 40.7720266]
+          },
+        'borough': 'Manhattan',
+        'cuisine': 'Italian',
+        'name': 'Vella',
+        'id': 41704620,
+        'grades': [{'date': datetime.datetime(2014, 12, 22, 0, 0), 'grade': 'A', 'score': 10}]
 
-cur.execute("DROP TABLE IF EXISTS accounts")
-con.commit()
+    })
 
-cur.execute("""CREATE TABLE accounts (
-                    username VARCHAR(50) PRIMARY KEY,
-                    fullname VARCHAR(50) NOT NULL,
-                    balance INTEGER DEFAULT 0,
-                    group_id INT NOT NULL DEFAULT 0
-                );
-                COMMIT;""")
+    print(result)
 
-data = [('jones', 'Alice Jones', 82, 1),
-        ('bitdiddl', 'Ben Bitdiddle', 65, 1),
-        ('mike', 'Michael Dole', 73, 2),
-        ('alysaa', 'Alyssa P. Hacker', 79, 3),
-        ('bbrown', 'Bob Brown', 100, 3)]
-
-for entry in data:
-    cur.execute(f"""INSERT INTO accounts (username, fullname, balance, group_id)
-                    VALUES(
-                        '{entry[0]}',
-                        '{entry[1]}',
-                        {entry[2]},
-                        {entry[3]}    
-                    );
-                    COMMIT;""")
-
-cur.execute("SELECT * FROM accounts;")
-output = cur.fetchall()
-for line in output:
-    print(line)
+ex2()
